f6df4e28a0004f766f632c97a73315d5a82959ff
bsse25032 <bsse25032@itu.edu.pk>
2026-03-13 23:34:39 +0500

    updated

--- main.cpp
+++ main.cpp
@@ -1,14 +1,29 @@
-#include "Hero.h"
+#include"Hero.h"
+#include"Guild.h"
 #include "Warrior.h"
 #include "Knight.h"
-#include "Guild.h"
-#include <iostream>
+#include "Spellblade.h"
+using namespace std;
+int main() {
+    //Initializing the Guild
+    Guild myGuild("Pak Defenders xD");
+    //Creating Heroes using dynamic memory allocation 
+    Hero* h1 = new Warrior("Mavra", 100, 50, 20);
+    Hero* h2 = new Knight("Rohma", 120, 60, 25, 30);
+    Hero* h3 = new Spellblade("Minahil", 80, 40, 10, 100, 70);
+    //Recruiting heroes using the overloading += operator
+    myGuild+=h1;
+    myGuild+=h2;
+    myGuild+=h3;
+    //Printing the entire guild roster using the overloading << operator
+    cout<<"*** Guild Status ***"<<endl;
+    cout<<myGuild<<endl;
 
-int main()
-{
-    Hero h1("Mavra",100,50);
-    Warrior w("Gimli",100,40,10);
-    Knight k("Arthur", 150, 60, 20, 25);
-
+    // Hero operations
+    cout<<"*** Testing Hero Operators ***"<<endl;
+    if (*h2>*h1) {
+        cout<<h2->getName()<<" is stronger than "<<h1->getName()<<".\n";
+    }
+    cout<<"Combined Vitality of Mavra and Rohma: "<<(*h1 + *h2)<<"\n\n";
     return 0;
 }
